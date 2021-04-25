f1a3fc0499e0d0a0259032649239469903907c3c
SkillCode <68539499+SkillC0de@users.noreply.github.com>
2021-04-25 19:08:19 +0200

    Update Constants.java

--- src/main/java/net/skillcode/simpleguiceplugin/utils/Constants.java
+++ src/main/java/net/skillcode/simpleguiceplugin/utils/Constants.java
@@ -3,6 +3,6 @@ package net.skillcode.simpleguiceplugin.utils;
 public class Constants {
 
     public static final String BUILD_MODE_ON = "§aYou can build now.";
-    public static final String BUILD_MODE_OFF = "§cYou can't build any more.";
+    public static final String BUILD_MODE_OFF = "§cYou can't build anymore.";
 
 }
